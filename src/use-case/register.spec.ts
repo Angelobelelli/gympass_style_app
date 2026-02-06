a4b79b1a0c1b08818fedf3454c9b4b556f72a563
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2026-02-06 16:30:57 -0300

    Primeiro Commit

---
+++ src/use-case/register.spec.ts
@@ -0,0 +1,54 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
+
+let userRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+describe('Register Use Case', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(userRepository)
+  })
+
+  it('should be able to register', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'Angelobelelli@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should  hash user passqord upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'Angelobelelli@gmail.com',
+      password: '123456',
+    })
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should  not be able to register with same email twice', async () => {
+    const email = 'Angelobelelli@gmail.com'
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
