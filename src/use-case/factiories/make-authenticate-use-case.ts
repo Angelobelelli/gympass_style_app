d68cad22dea8d64265785359cea1a10057e1d1eb
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2026-02-06 16:31:56 -0300

    Revert "Primeiro Commit"
    This reverts commit a4b79b1a0c1b08818fedf3454c9b4b556f72a563.

--- src/use-case/factiories/make-authenticate-use-case.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUserCase } from '../authenticate'
-
-export function makeAuthenticateUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const authenticateUserCase = new AuthenticateUserCase(usersRepository)
-
-  return authenticateUserCase
-}
