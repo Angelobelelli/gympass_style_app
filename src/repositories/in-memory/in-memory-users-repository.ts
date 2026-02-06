ec45cbe4e79d753a87412c49d4110d76ef2db5a5
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2026-02-06 16:43:44 -0300

    Primeiro Commit

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,28 @@
+import { User, Prisma } from '@prisma/client'
+import { UserRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UserRepository {
+  public items: User[] = []
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id)
+    return user || null
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+    return user || null
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: String(this.items.length + 1), // ou use um uuid
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    } as User
+    this.items.push(user)
+    return user
+  }
+}
